75cf7e3ce2c9212a147b86820eec8961d27261e6
Karthikeyan N <109551096+Karthikoffl@users.noreply.github.com>
2023-05-26 17:40:39 +0530

    (2605) commit

--- screens/Home.js
+++ screens/Home.js
@@ -1,5 +1,5 @@
 import { SafeAreaView, View, Text, ScrollView } from 'react-native'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import HeaderTabs from '../components/HeaderTabs'
 import SearchBar from '../components/SearchBar'
 import Categories from '../components/Categories'
@@ -8,16 +8,38 @@ import RestaurantItems, { localRestaurants } from '../components/RestaurantItems
 const YELP_API_KEY = "uwPPDZx1pBbbJEYJFMC2SKdBUb-6QXhntKXsmryPVbC1yfPfyxeldqzwHBQjWTBg273AaNZpASBhLwkH9mAT4c-ArCGutf1G5Pkny-7LYeWALBgdIcOW9PIzo4pwZHYx";
 
 export default function Home() {
+
   const [restaurantData, setRestaurantData] = useState(localRestaurants);
+  const [city, setCity] = useState("Chennai");
+  const [activeTab, setActiveTab] = useState("Delivery");
+
   const getRestaurantsFromYelp = () => {
     const yelpUrl = `https://api.yelp.com/v3/businesses/search?term=restaurants&location=${city}`;
-  }
+
+  const apiOptions = {
+    headers: {
+      Authorization: `Bearer ${YELP_API_KEY}`,
+    },
+  };
+    return fetch(yelpUrl, apiOptions)
+      .then((res) => res.json())
+      .then(json => setRestaurantData(
+        json.businesses.filter((business) => 
+            business.transactions.includes(activeTab.toLowerCase())
+          )
+        )
+      );
+  };
+
+  useEffect(() => {
+    getRestaurantsFromYelp();
+  }, [city, activeTab]);
 
   return (
     <SafeAreaView style={{backgroundColor: "#eee", flex: 1}}>
       <View style={{backgroundColor: "white", padding: 15}}>
-        <HeaderTabs />
-        <SearchBar />
+        <HeaderTabs activeTab={activeTab} setActiveTab={setActiveTab} />
+        <SearchBar  cityHandler={setCity}  />
       </View>
       <ScrollView showsVerticalScrollIndicator={false}>
         <Categories />
