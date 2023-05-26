54384717331ab8e5bb7e9baf7d06d285ff1c0295
Karthikeyan N <109551096+Karthikoffl@users.noreply.github.com>
2023-05-26 13:52:11 +0530

    (2605) commit

--- screens/Home.js
+++ screens/Home.js
@@ -1,11 +1,21 @@
-import { SafeAreaView, View, Text } from 'react-native'
+import { SafeAreaView, View, Text, ScrollView } from 'react-native'
 import React from 'react'
 import HeaderTabs from '../components/HeaderTabs'
+import SearchBar from '../components/SearchBar'
+import Categories from '../components/Categories'
+import RestaurantItem from '../components/RestaurantItem'
 
 export default function Home() {
   return (
-    <SafeAreaView>
-      <HeaderTabs />
+    <SafeAreaView style={{backgroundColor: "#eee", flex: 1}}>
+      <View style={{backgroundColor: "white", padding: 15}}>
+        <HeaderTabs />
+        <SearchBar />
+      </View>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <Categories />
+        <RestaurantItem />
+      </ScrollView>
     </SafeAreaView>
   )
 }
